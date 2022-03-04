1e4e0ca4e4241bcae38e41e97ca79e7760ad0f60
Meena Haque <aneemqah@gmail.com>
2022-03-04 09:42:19 +0000

    add blockquote

--- client/src/Views/Bookclubsearch.js
+++ client/src/Views/Bookclubsearch.js
@@ -1,39 +1,24 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function Bookclubsearch(props) {
-    
-    // store result request in state
-    // in return
-    // to map bookclubs and display
-    
-
-    return (
-        <div className='Bookclubsearch'>
-            <h2>My Bookclubs</h2>
-            {/* <div className="search">
-                <label>
-                <input
-                    type="text"
-                    name="extra_pic"
-                    value={item.extra_pic}
-                    onChange={handleInputChange}
-                />
-                </label>
-                
-            </div> */}
-
-
-            <ul>
-                {props.bookClubs.map((bc) => (
-                    <li className="navlink" key={bc.id}>
-                        <Link to={"/Bookclub/"+bc.id}>{bc.name}</Link>
-                    </li>
-            ))}
-            </ul> 
-            {/* <Link to="/Bookclub/:id">{bookClub.name}</Link> */}
-        </div>
-    )
+  return (
+    <div className="Bookclubsearch">
+      <h2>Bookclubs</h2>
+      <ul>
+        {props.bookClubs.map((bc) => (
+          <li className="navlink" key={bc.id}>
+            <Link to={"/Bookclub/" + bc.id}>{bc.name}</Link>
+          </li>
+        ))}
+      </ul>
+      {/* Create a new button so that user can add a new bookclub. This should be a form component. Add route to app.js */}
+      <div>
+        <Link key="create" to="/CreateBookclub" className="btn btn-success">
+          Create a Bookclub
+        </Link>
+      </div>
+    </div>
+  );
 }
-
