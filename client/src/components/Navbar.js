5b950a3595b6352b2e8cf2a5219f71b248ebd91e
Meena Haque <aneemqah@gmail.com>
2022-03-04 09:23:31 +0000

    fix styling on nav comp

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './Navbar.css';
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
-    return (
-        <nav className="Navbar">
-            <ul>
-                <li><NavLink to="/Bookclubs">Bookclubs</NavLink></li>
-                <li><NavLink to="/Books">Books</NavLink></li>
-                <li className="logout"><NavLink to="/">Log out</NavLink></li>
-            </ul>
-
-        </nav>
-    );
+  return (
+    <div className="title">
+      <nav className="nav-container">
+        <ul>
+          <li>
+            <NavLink to="/Bookclubs" className="link">
+              | Bookclubs |
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/Books" className="link">
+              Books |
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/" className="link">
+              Login |
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
